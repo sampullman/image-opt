c235c687b5da8f225594681fd7850df5186761e1
Sam Pullman <sampullman@berkeley.edu>
2025-08-10 17:47:00 +0000

    feat(copy-exif): Create new copy-exif package
    Add a workspace package, `@samatech/copy-exif`, to copy EXIF data between JPEGs.
    
    - `readExif(jpeg: ArrayBuffer)`: Reads EXIF data from a JPEG and return as JSON.
    - `writeExif(jpeg: ArrayBuffer, exifData: string)`: Writes EXIF data to a JPEG.

--- packages/copy-exif/src/index.ts
+++ packages/copy-exif/src/index.ts
@@ -1,1 +1,37 @@
+import piexif from 'piexifjs'
+
 export * from './copy-exif'
+
+function arrayBufferToDataUrl(buffer: ArrayBuffer): string {
+  const base64 = Buffer.from(buffer).toString('base64')
+  return `data:image/jpeg;base64,${base64}`
+}
+
+function dataUrlToArrayBuffer(dataUrl: string): ArrayBuffer {
+  const base64 = dataUrl.replace(/^.*?,/, '')
+  const buffer = Buffer.from(base64, 'base64')
+  return buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength)
+}
+
+export function readExif(jpeg: ArrayBuffer): string | null {
+  const dataUrl = arrayBufferToDataUrl(jpeg)
+  const exifData = piexif.load(dataUrl)
+
+  if (
+    Object.keys(exifData['0th']).length === 0 &&
+    Object.keys(exifData['Exif']).length === 0 &&
+    Object.keys(exifData['GPS']).length === 0 &&
+    Object.keys(exifData['1st']).length === 0
+  ) {
+    return null
+  }
+  return JSON.stringify(exifData)
+}
+
+export function writeExif(jpeg: ArrayBuffer, exifData: string): ArrayBuffer {
+  const dataUrl = arrayBufferToDataUrl(jpeg)
+  const exifObj = JSON.parse(exifData)
+  const newExifDump = piexif.dump(exifObj)
+  const newDataUrl = piexif.insert(newExifDump, dataUrl)
+  return dataUrlToArrayBuffer(newDataUrl)
+}
